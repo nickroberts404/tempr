84b5e83b7e99488e6daa460a326c1c3deb35efc0
nickroberts404 <nickroberts404@gmail.com>
2016-01-26 13:40:24 -0600

    Templates now work

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 var inq = require('inquirer');
 var exec = require('child_process').exec;
 var options = require('./options.js');
+var path = require('path');
 var cwd = process.cwd();
 var arg = process.argv.slice(2);
 
@@ -11,16 +12,11 @@ var templates = options.get_templates();
 var questions = [
 	{type: 'list', name: 'template', message: 'Choose a template..', choices: options.get_choices},
 	{type: 'input', name: 'fname', message: 'Name of directory?', when: (ans)=> templates[ans.template].is_dir },
-	{type: 'input', name: 'fname', message: 'Name of file?', when: (ans)=> !templates[ans.template].is_dir, filter: filter_fname }
+	{type: 'input', name: 'fname', message: 'Name of file? (no extension)', when: (ans)=> !templates[ans.template].is_dir}
 ]
 
-function filter_fname(a, b){
-	console.log(a, b);
-}
-
 inq.prompt(questions, function(answers){
-	console.log(answers);
+	var template = templates[answers.template];
+	var ext = template.is_dir ? '' : path.extname(template.dirname);
+	exec(`cp ${template.is_dir ? '-r' : ''} ${template.dirname} ${cwd+'/'+answers.fname+ext}`); 
 });
-
-// console.log(`cp ${is_dir ? '-r' : ''} ${dict[arg[0]]} ${cwd + '/' + arg[1]}`);
-// exec(`cp ${is_dir ? '-r' : ''} ${dict[arg[0]]} ${cwd + '/' + arg[1]}`);
